18d2920d82958921275a6b5efd9037a0c1b1dfdb
Projeto-integrador-2019.2 <mateussantanacabral@gmail.com>
2020-10-07 21:27:12 -0300

    Primeira versão

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment'; 
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    HttpClientModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
+    ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/sair/sair.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, MenuController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth/auth';
+
+@Component({
+  selector: 'app-sair',
+  templateUrl: './sair.page.html',
+  styleUrls: ['./sair.page.scss'],
+})
+export class SairPage implements OnInit {
+  
+  constructor(private navCtrl : NavController,
+  private auth: AngularFireAuth,
+  private menuCtrl : MenuController) { }
+  ngOnInit() {
+  this.logout();
+  }
+ 
+  logout(){
+  this.auth.signOut().then(data=>{
+  this.menuCtrl.enable(false);
+  this.navCtrl.navigateRoot(['/login']);
+  })
+  }
+ } 
+ 
